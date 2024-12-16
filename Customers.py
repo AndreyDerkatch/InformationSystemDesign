34888e1430843132a518b9bc86647571d58f71c9
AndreyDerkatch <112868448+AndreyDerkatch@users.noreply.github.com>
2024-12-16 17:43:19 +0300

    Update Customers.py

--- Customers.py
+++ Customers.py
@@ -1,22 +1,26 @@
 import re
 from datetime import datetime
 
+from CustomerShortInfo import CustomerShortInfo
 
-class Customer:
-    def __init__(self, customer_id, first_name, last_name, email, phone_number, address, city, 
-                 postal_code, country, date_joined):
-        self.__customer_id = self.__validate_id(customer_id)
-        self.__first_name = self.__validate_name(first_name)
-        self.__last_name = self.__validate_name(last_name)
-        self.__email = self.__validate_email(email)
-        self.__phone_number = self.__validate_phone_number(phone_number)
-        self.__address = self.__validate_non_empty_string(address, "Address")
-        self.__city = self.__validate_non_empty_string(city, "City")
-        self.__postal_code = self.__validate_postal_code(postal_code)
-        self.__country = self.__validate_non_empty_string(country, "Country")
-        self.__date_joined = self.__validate_date_joined(date_joined)
 
-    # Статический метод для создания объекта из строки
+class Customer(CustomerShortInfo):
+    def __init__(self, first_name, last_name, email, customer_id=None, phone_number=None,
+                 address=None, city=None, postal_code=None, country=None, date_joined=None):
+        super().__init__(customer_id=customer_id, first_name=first_name, last_name=last_name, email=email)
+        if phone_number:
+            self.set_phone_number(phone_number)
+        if address:
+            self.set_address(address)
+        if city:
+            self.set_city(city)
+        if postal_code:
+            self.set_postal_code(postal_code)
+        if country:
+            self.set_country(country)
+        if date_joined:
+            self.set_date_joined(date_joined)
+
     @staticmethod
     def from_string(data_str):
         try:
@@ -24,6 +28,7 @@ class Customer:
             data = data_str.split(',')
             if len(data) != 10:
                 raise ValueError("Incorrect number of fields in the string.")
+
             customer_id = int(data[0].strip())
             first_name = data[1].strip()
             last_name = data[2].strip()
@@ -34,52 +39,72 @@ class Customer:
             postal_code = int(data[7].strip())
             country = data[8].strip()
             date_joined = data[9].strip()
-            # Создание и возврат объекта Customer
-            return Customer(customer_id, first_name, last_name, email, phone_number, address, city, postal_code,
-                            country, date_joined)
+
+            return Customer(customer_id=customer_id, first_name=first_name, last_name=last_name, email=email,
+                            phone_number=phone_number, address=address, city=city, postal_code=postal_code,
+                            country=country, date_joined=date_joined)
         except Exception as e:
             raise ValueError(f"Error parsing customer data: {e}")
-    
-    # Getters
-    def get_customer_id(self):
-        return self.__customer_id
 
-    def get_first_name(self):
-        return self.__first_name
+    @staticmethod
+    def from_dict(data: dict):
+        date_time = datetime.strptime(data['date_joined'], '%Y-%m-%d %H:%M:%S') if data.get('date_joined') else None
+        return Customer(
+            customer_id=data['customer_id'],
+            first_name=data['first_name'],
+            last_name=data['last_name'],
+            email=data['email'],
+            phone_number=data.get('phone_number'),
+            address=data.get('address'),
+            city=data.get('city'),
+            postal_code=data.get('postal_code'),
+            country=data.get('country'),
+            date_joined=date_time
+        )
 
-    def get_last_name(self):
-        return self.__last_name
-
-    def get_email(self):
-        return self.__email
+    def to_dict(self):
+        return {
+            'customer_id': self.get_customer_id(),
+            'first_name': self.get_first_name(),
+            'last_name': self.get_last_name(),
+            'email': self.get_email(),
+            'phone_number': self.get_phone_number(),
+            'address': self.get_address(),
+            'city': self.get_city(),
+            'postal_code': self.get_postal_code(),
+            'country': self.get_country(),
+            'date_joined': self.get_date_joined().strftime('%Y-%m-%d %H:%M:%S') if self.get_date_joined() else None
+        }
 
     def get_phone_number(self):
-        return self.__phone_number
+        if hasattr(self, '_Customer__phone_number'):
+            return self.__phone_number
+        return None
 
     def get_address(self):
-        return self.__address
+        if hasattr(self, '_Customer__address'):
+            return self.__address
+        return None
 
     def get_city(self):
-        return self.__city
+        if hasattr(self, '_Customer__city'):
+            return self.__city
+        return None
 
     def get_postal_code(self):
-        return self.__postal_code
+        if hasattr(self, '_Customer__postal_code'):
+            return self.__postal_code
+        return None
 
     def get_country(self):
-        return self.__country
+        if hasattr(self, '_Customer__country'):
+            return self.__country
+        return None
 
     def get_date_joined(self):
-        return self.__date_joined
-
-    # Setters с валидацией
-    def set_first_name(self, first_name):
-        self.__first_name = self.__validate_name(first_name)
-
-    def set_last_name(self, last_name):
-        self.__last_name = self.__validate_name(last_name)
-
-    def set_email(self, email):
-        self.__email = self.__validate_email(email)
+        if hasattr(self, '_Customer__date_joined'):
+            return self.__date_joined
+        return None
 
     def set_phone_number(self, phone_number):
         self.__phone_number = self.__validate_phone_number(phone_number)
@@ -98,26 +123,6 @@ class Customer:
 
     def set_date_joined(self, date_joined):
         self.__date_joined = self.__validate_date_joined(date_joined)
-
-    # Статические методы валидации
-    @staticmethod
-    def __validate_id(customer_id):
-        if isinstance(customer_id, int) and customer_id > 0:
-            return customer_id
-        raise ValueError("Customer ID must be a positive integer.")
-
-    @staticmethod
-    def __validate_name(name):
-        if isinstance(name, str) and name:
-            return name
-        raise ValueError("Name must be a non-empty string up to 255 characters.")
-
-    @staticmethod
-    def __validate_email(email):
-        email_regex = r"[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+"
-        if isinstance(email, str) and re.match(email_regex, email):
-            return email
-        raise ValueError("Invalid email format.")
 
     @staticmethod
     def __validate_phone_number(phone_number):
@@ -148,26 +153,21 @@ class Customer:
         except ValueError:
             raise ValueError("Date Joined must be a valid datetime in the format YYYY-MM-DD HH:MM:SS.")
 
- # Полная версия объекта
     def __str__(self):
-        return (f"Customer [ID: {self.__customer_id}, Name: {self.__first_name} {self.__last_name}, "
-                f"Email: {self.__email}, Phone: {self.__phone_number}, Address: {self.__address}, "
-                f"City: {self.__city}, Postal Code: {self.__postal_code}, Country: {self.__country}, "
-                f"Date Joined: {self.__date_joined.strftime('%Y-%m-%d %H:%M:%S')}]")
-    # Краткая версия объекта (ключевая информация)
+        return (f"Customer [ID: {self.get_customer_id()}, "
+                f"Name: {self.get_first_name()} {self.get_last_name()}, "
+                f"Email: {self.get_email()}, "
+                f"Phone: {self.get_phone_number()}, "
+                f"Address: {self.get_address()}, "
+                f"City: {self.get_city()}, "
+                f"Postal Code: {self.get_postal_code()}, "
+                f"Country: {self.get_country()}, "
+                f"Date Joined: {self.get_date_joined()}]")
+
     def short_info(self):
-        return f"Customer [ID: {self.__customer_id}, Name: {self.__first_name} {self.__last_name}, Email: {self.__email}]"
-    # Метод для сравнения объектов
+        return super().__str__()
+
     def __eq__(self, other):
         if isinstance(other, Customer):
-            return (self.__customer_id == other.__customer_id and
-                    self.__first_name == other.__first_name and
-                    self.__last_name == other.__last_name and
-                    self.__email == other.__email and
-                    self.__phone_number == other.__phone_number and
-                    self.__address == other.__address and
-                    self.__city == other.__city and
-                    self.__postal_code == other.__postal_code and
-                    self.__country == other.__country and
-                    self.__date_joined == other.__date_joined)
+            return super().__eq__(other)
         return False
