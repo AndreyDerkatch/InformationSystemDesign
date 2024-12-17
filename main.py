d224a673b368c4911da05573890f9728a98adaa6
AndreyDerkatch <112868448+AndreyDerkatch@users.noreply.github.com>
2024-12-17 16:40:42 +0300

    Изменение функций сравнение объектов на равенство

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import tkinter as tk
 from Customer import Customer
 
 if __name__ == "__main__":
-    
-    Cust1 = Customer("Андрей", "Деркач", "andrey.derkach@mail.ru")
-    Cust2 = Customer("Максим", "Баландин", "max@mail.ru")
-    
+
+    Cust1 = Customer("Андрей", "Деркач", "89283334455")
+    Cust2 = Customer("Максим", "Баландин", "89283334555")
+
     print(Cust1)
     print(Cust2)
     print(Cust1.__eq__(Cust2))
