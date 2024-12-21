d47b7492fe589fc30e50b6e96064b6529753ad4b
AndreyDerkatch <112868448+AndreyDerkatch@users.noreply.github.com>
2024-12-21 12:55:37 +0300

    Delete FileStrategy.py

--- FileStrategy.py
+++
@@ -1,6 +0,0 @@
-class FileStrategy:
-    def read_all(self):
-        raise NotImplementedError("Метод должен быть реализован в конкретной стратегии")
-
-    def save_all(self, customers):
-        raise NotImplementedError("Метод должен быть реализован в конкретной стратегии")
